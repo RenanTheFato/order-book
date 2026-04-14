ba3ba9bcc1d7dfa7d7a66934d5b904e4b20bc8fc
RenanTheFato <renan.santana007@hotmail.com>
2026-04-14 18:02:26 -0300

    fix issue on load env variables

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client.js";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!
