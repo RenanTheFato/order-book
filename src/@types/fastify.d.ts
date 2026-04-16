b48bb8b8574eed645c76eb5f53422a952be1880e
RenanTheFato <renan.santana007@hotmail.com>
2026-04-16 09:06:26 -0300

    remove unnecessary fields to store a session

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -7,9 +7,6 @@ declare module 'fastify' {
       email: string,
       name: string,
       last_name: string
-      hashed_password: string,
-      created_at: Date,
-      updated_at: Date,
     }>
   }
 }
