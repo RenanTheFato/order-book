04dada59cca394edaf478971ea809e13da8eeea2
RenanTheFato <renan.santana007@hotmail.com>
2026-04-16 15:22:45 -0300

    create error tool to avoid repetitive validations

---
+++ src/errors/index.ts
@@ -0,0 +1,20 @@
+export class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = this.constructor.name
+  }
+}
+
+export class InvalidOperationError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = this.constructor.name
+  }
+}
+
+export class BadRequestError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = this.constructor.name
+  }
+}
