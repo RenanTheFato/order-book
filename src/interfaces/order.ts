bd35e226e15b2785e92af05ec06c17445bd4e02c
RenanTheFato <renan.santana007@hotmail.com>
2026-04-21 11:02:08 -0300

    add order data types

---
+++ src/interfaces/order.ts
@@ -0,0 +1,16 @@
+import { OrderSide, OrderStatus, OrderType } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/client";
+
+export interface Order {
+  id: string,
+  user_id: string,
+  asset_id: string,
+  side: OrderSide,
+  type: OrderType,
+  status: OrderStatus,
+  price: Decimal | null,
+  quantity: Decimal,
+  quantity_filled: Decimal,
+  created_at: Date,
+  updated_at: Date
+}
